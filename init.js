45a3b2803ca72c022ce051ff37867031a41440d7
Amadej Krepek <krepekamadej@gmail.com>
2021-03-20 20:04:27 +0100

    Imported server file

--- init.js
+++ init.js
@@ -1,4 +1,8 @@
+// the entry file
+import server from "./server.js";
+/* Only needed for insterting data in database
 import Race from "./controllers/Race";
 Race.Insert("ironman");
 Race.Insert("ironman70");
 Race.Insert("ultra");
+*/
