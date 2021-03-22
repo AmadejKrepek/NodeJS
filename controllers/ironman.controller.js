847ee85a14c261f05e42c9f455744f868e4c2ae5
Amadej Krepek <krepekamadej@gmail.com>
2021-03-22 15:21:12 +0100

    Added create and find one by id method

--- controllers/ironman.controller.js
+++ controllers/ironman.controller.js
@@ -1,9 +1,33 @@
 const db = require("../models");
-const ironman = db.ironman;
+const Athlete = db.ironman;
 
-// Create and Save a new ironman
+// Create and Save a new Athlete
 exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.name) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
 
+  // Create a Tutorial
+  const athlete = new Athlete({
+    name: req.body.name,
+    country: req.body.country,
+    runDistance: req.body.runDistance
+  });
+
+  // Save Tutorial in the database
+  athlete
+    .save(athlete)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
 };
 
 // Retrieve all ironmans from the database.
@@ -11,7 +35,7 @@ exports.findAll = (req, res) => {
   const name = req.query.name;
   var condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {};
 
-  ironman.find(condition)
+  Athlete.find(condition)
     .then(data => {
       res.send(data);
     })
@@ -23,17 +47,29 @@ exports.findAll = (req, res) => {
     });
 };
 
-// Find a single ironman with an id
+// Find a single Athlete with an id
 exports.findOne = (req, res) => {
+  const id = req.params.id;
 
+  Athlete.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + id });
+    });
 };
 
-// Update a ironman by the id in the request
+// Update a Athlete by the id in the request
 exports.update = (req, res) => {
 
 };
 
-// Delete a ironman with the specified id in the request
+// Delete a Athlete with the specified id in the request
 exports.delete = (req, res) => {
 
 };
