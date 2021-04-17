bfb6c0f734a0afee00f4627bcf40798100051751
Amadej Krepek <krepekamadej@gmail.com>
2021-04-17 17:54:15 +0200

    Implemented new model for ultras

--- models/ultra.model.js
+++ models/ultra.model.js
@@ -1,3 +1,4 @@
+/*
 export default mongoose => {
     const { Schema } = mongoose
     const MyModel = mongoose.model('ultras', new Schema(
@@ -6,7 +7,7 @@ export default mongoose => {
             Overall: String,
             Competitor: String,
             Country: String,
-            Age_Category: Number,
+            Age_Category: String,
             Swim: String,
             Trans1: String,
             Bike: String,
@@ -18,3 +19,35 @@ export default mongoose => {
     ));
     return MyModel;
 };
+*/
+
+//To je nova verzija, če ne bo delovala prešaltaj na staro!
+const mongoosePaginate = require("mongoose-paginate-v2");
+export default mongoose => {
+    const schema = new mongoose.Schema({
+        Rank: Number,
+        Overall: String,
+        Competitor: String,
+        Country: String,
+        Age_Category: String,
+        Swim: String,
+        Trans1: String,
+        Bike: String,
+        Trans2: String,
+        Run: String,
+        Finish: String,
+        Comment: String,
+      });
+       
+      schema.plugin(mongoosePaginate);
+       
+      const MyModel = mongoose.model("ultras", schema);
+
+      MyModel.paginate({}, { limit: 5 })
+      .then(result => {
+          //console.log(result);
+      })
+      .catch(err => console.log(err));
+
+      return MyModel;
+}
